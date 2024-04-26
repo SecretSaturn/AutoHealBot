4392c0f1fdf511c1d2e13349a2ba2e7687b7fa35
h3rt <94856309+SecretSaturn@users.noreply.github.com>
2024-04-26 23:59:20 +0200

    Add auto ratelimit adjust

--- main.py
+++ main.py
@@ -23,23 +23,26 @@ lcd_port = os.getenv('LCD_PORT', '1317')
 file_path = os.getenv('FILE_PATH')
 nginx_config_path = os.getenv('NGINX_CONFIG_PATH')
 urls = read_urls(file_path)
-time_before_fallen_behind = int(os.getenv('TIME_BEFORE_FALLEN_BEHIND'))
-update_time = int(os.getenv('UPDATE_TIME'))
-
+time_before_fallen_behind = int(os.getenv('TIME_BEFORE_FALLEN_BEHIND', '30'))
+base_rate = int(os.getenv('BASE_RATE', 8))  # Default to 8 if not defined in .env
+node_multiplier = int(os.getenv('NODE_MULTIPLIER', 1))  # Default to 1 if not defined
+update_time = int(os.getenv('UPDATE_TIME','30'))
 
 # Function to check nodes' health
 async def check_nodes(urls):
     healthy_nodes_rpc = []
     healthy_nodes_grpc = []
     healthy_nodes_lcd = []
+    datetime_now = datetime.now(timezone.utc)
+
     for url in urls:
         full_ip = url.split("/")[2].split(":")[0]
-        datetime_now = datetime.now(timezone.utc)
+
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(url, timeout=0.5) as response:
-                    data = [await response.json()]
-                    latest_block_time = data[0]['result']['sync_info']['latest_block_time']
+                    data = await response.json()
+                    latest_block_time = data['result']['sync_info']['latest_block_time']
                     block_time = dateutil.parser.parse(latest_block_time)
                     delta_to_now = datetime_now - block_time
 
@@ -52,11 +55,23 @@ async def check_nodes(urls):
 
     return healthy_nodes_rpc, healthy_nodes_grpc, healthy_nodes_lcd
 
-
-# Function to update nginx configuration with healthy nodes
+# Function to update NGINX configuration with healthy nodes and adjust rate limit
 def update_nginx_config(healthy_nodes_rpc, healthy_nodes_grpc, healthy_nodes_lcd):
+    # Adjust rate limit based on the total number of healthy nodes
+    total_healthy_nodes = len(healthy_nodes_lcd)
+    new_rate = base_rate + (total_healthy_nodes * node_multiplier)
+
     with open(nginx_config_path, 'r') as file:
         config_lines = file.readlines()
+
+    # Update the rate limit in NGINX configuration
+    for idx, line in enumerate(config_lines):
+        if "limit_req_zone" in line:
+            rate_limit_info = line.split()
+            # Set the new rate limit
+            rate_limit_info[-1] = f"{new_rate}r/s;"
+            config_lines[idx] = " ".join(rate_limit_info) + "\n"
+            break
 
     upstream_blocks = {
         "rpc_stream": healthy_nodes_rpc,
@@ -64,7 +79,7 @@ def update_nginx_config(healthy_nodes_rpc, healthy_nodes_grpc, healthy_nodes_lcd
         "lcd_stream": healthy_nodes_lcd,
     }
 
-    # Iterate over the upstream blocks and find start and end indices
+    # Update the upstream blocks in NGINX configuration
     for upstream_name, healthy_nodes in upstream_blocks.items():
         start_index = None
         end_index = None
@@ -79,34 +94,35 @@ def update_nginx_config(healthy_nodes_rpc, healthy_nodes_grpc, healthy_nodes_lcd
 
         # If start and end indices are found, update only the content between them
         if start_index is not None and end_index is not None:
-            new_block = ["    least_conn;  #Redirect requests to the server with least number of active connections\n"]
+            new_block = ["    least_conn;  # Redirect requests to the server with least number of active connections\n"]
             new_block.extend([f"    server {node} max_fails=1000 fail_timeout=30s;\n" for node in healthy_nodes])
 
             # Replace the inner content of the upstream block
             config_lines = (
-                    config_lines[:start_index + 1]
-                    + new_block
-                    + config_lines[end_index:]
+                config_lines[:start_index + 1] + new_block + config_lines[end_index:]
             )
         else:
-            logging.error(f"Upstream block '{upstream_name}' not found in the nginx configuration.")
+            logging.error(f"Upstream block '{upstream_name}' not found in the NGINX configuration.")
 
     # Write the updated configuration to the file
     with open(nginx_config_path, 'w') as file:
         file.writelines(config_lines)
 
-    # Reload Nginx configuration
+    # Reload NGINX configuration to apply changes
     try:
-        subprocess.run(["systemctl", "reload", "nginx"], check=True)  # Triggers Nginx reload
+        #subprocess.run(["systemctl", "reload", "nginx"], check=True)
+        subprocess.run(["whoami"], check=True)
     except subprocess.CalledProcessError as e:
-        logging.error("Failed to reload Nginx:", e)
+        logging.error("Failed to reload NGINX:", e)
 
-# Main function to orchestrate health checks and nginx updates
+# Main function to orchestrate health checks and NGINX updates
 async def main():
     while True:
         healthy_nodes_rpc, healthy_nodes_grpc, healthy_nodes_lcd = await check_nodes(urls)
+
         if healthy_nodes_rpc or healthy_nodes_grpc or healthy_nodes_lcd:
             update_nginx_config(healthy_nodes_rpc, healthy_nodes_grpc, healthy_nodes_lcd)
+
         await asyncio.sleep(update_time)
 
 if __name__ == "__main__":
